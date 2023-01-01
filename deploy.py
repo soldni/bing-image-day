0ee557de0d946546d6749fa59fbb47d4c9f71f99
Luca Soldaini <lucas@allenai.org>
2022-12-31 16:06:24 -0800

    https'ed

--- deploy.py
+++ deploy.py
@@ -4,12 +4,12 @@ import urllib.request
 
 def main():
     req = urllib.request.Request(
-        'http://www.bing.com/HPImageArchive.aspx'
+        'https://www.bing.com/HPImageArchive.aspx'
         '?format=js&idx=0&n=1&mkt=en-US'
     )
     with urllib.request.urlopen(req) as response:
         data = json.loads(response.read())
-        url = 'http://www.bing.com' + data['images'][0]['url']
+        url = 'https://www.bing.com' + data['images'][0]['url']
         title = data['images'][0]['title']
         with open('template.html', 'r') as f, \
                 open('docs/index.html', 'w') as f2:
