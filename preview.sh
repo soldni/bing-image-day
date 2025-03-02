8aa8ac0301314a7904c784e2ff0b7a6e4b8b25ca
Luca Soldaini <luca@soldaini.net>
2025-03-01 17:49:25 -0800

    preview script

--- preview.sh
+++ preview.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python -m http.server 8000 --directory docs --bind localhost
+# Get script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+python "${SCRIPT_DIR}/deploy.py" && python -m http.server 8000 --directory "${SCRIPT_DIR}/docs" --bind localhost
